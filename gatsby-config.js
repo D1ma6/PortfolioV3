ecd957c744df4e03e634c2d4abd34be3b1ffd2db
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-02-12 12:51:03 +0000

    change

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: "https://dmytrobula.com/",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "projects",
